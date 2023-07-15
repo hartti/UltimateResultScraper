0259217006d3d9c695f9a2b6344d71c98b857909
hartti <hartti@gmail.com>
2023-07-14 23:16:05 -0700

    Removed unnecessary print

--- Sources/UltimateResultScraper/GameScraperPelikone.swift
+++ Sources/UltimateResultScraper/GameScraperPelikone.swift
@@ -52,7 +52,6 @@ class GameScraperPelikone: GameScraper {
         }
         let number = try cells[0].html()
         let name = try cells[0].select("a").html()
-        print(name)
         let temp = ScrapedPlayer(name: name, jerseyNumber: number)
         players.append(temp)
       }
